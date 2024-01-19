708f296d9313c5f2f900e235824eb61a77ed871e
githinjisamson1 <githinjisamson148@gmail.com>
2024-01-19 15:56:52 +0300

    Set up basic flask app configurations

---
+++ server/config.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask
+from flask_Migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from flask_resftul import Api
+from dotenv import load_dotenv
+load_dotenv()
+
+
+app = Flask(__name__)
+
+app.secret_key = os.environ.get("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///pizzas.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.json.compact = False
+
+db = SQLAlchemy()
+migrate = Migrate(app, db)
+db.init_app(app)
+api = Api(app)
