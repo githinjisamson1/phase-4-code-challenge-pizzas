1de3391781d63c380938f5bc085ec91b182055f7
githinjisamson1 <githinjisamson148@gmail.com>
2024-01-19 19:10:45 +0300

    Implement serialization to prevent cases of recursion

--- server/models.py
+++ server/models.py
@@ -1,7 +1,8 @@
 from config import db
+from sqlalchemy_serializer import SerializerMixin
 
 
-class Restaurant(db.Model):
+class Restaurant(db.Model, SerializerMixin):
     __tablename__ = "restaurants"
 
     id = db.Column(db.Integer, primary_key=True)
@@ -16,27 +17,35 @@ class Restaurant(db.Model):
     pizzas = db.relationship(
         "RestaurantPizza", back_populates="restaurants")
 
+    # serialize to prevent recursion
+    serialize_rules = ("-restaurant_pizzas.restaurant",)
+
     def __repr__(self):
         return f'''Restaurant {self.name} {self.address}'''
 
 
-class RestaurantPizza(db.Model):
+# Association table
+class RestaurantPizza(db.Model, SerializerMixin):
     __tablename__ = "restaurant_pizzas"
 
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Integer)
-    restaurant_id = db.Column(db.Integer, db.ForeignKey())
-    pizza_id = db.Column(db.Integer, db.ForeignKey())
+    restaurant_id = db.Column(db.Integer, db.ForeignKey("restaurants.id"))
+    pizza_id = db.Column(db.Integer, db.ForeignKey("pizzas.id"))
 
-    
+    # relationship
     restaurants = db.relationship("Restaurant", back_populates="pizzas")
     pizzas = db.relationship("Pizza", back_populates="restaurants")
+
+    # serialize to prevent recursion
+    serialize_rules = ("-restaurant.restaurant_pizzas",
+                       "-pizza.restaurant_pizas",)
 
     def __repr__(self):
         return f'''RestaurantPizza {self.price}'''
 
 
-class Pizza(db.Model):
+class Pizza(db.Model, SerializerMixin):
     __tablename__ = "pizzas"
 
     id = db.Column(db.Integer, primary_key=True)
@@ -46,10 +55,13 @@ class Pizza(db.Model):
     # one to many
     restaurant_pizzas = db.relationship(
         "RestaurantPizza", backref="pizza")
-    
+
     # many to many via association table restaurant_pizzas
     restaurants = db.relationship(
         "RestaurantPizza", back_populates="pizzas")
+
+    # serialize to prevent recursion
+    serialize_rules = ("-restaurant_pizzas.pizza",)
 
     def __repr__(self):
         return f'''Pizza {self.name} {self.ingredients}'''
