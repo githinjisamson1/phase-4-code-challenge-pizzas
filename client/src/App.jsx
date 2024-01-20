29ee812c141ec9ef397ccc81fda370e09256af9a
githinjisamson1 <githinjisamson148@gmail.com>
2024-01-20 16:20:31 +0300

    Configure routes in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/homePage/Home";
+import About from "./pages/aboutPage/About";
+import Error from "./pages/errorPage/Error";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="*" element={<Error />} />
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ client/src/pages/errorPage/Error.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./error.css"
+
+const Error = () => {
+  return (
+    <div>Error</div>
+  )
+}
+
+export default Error
