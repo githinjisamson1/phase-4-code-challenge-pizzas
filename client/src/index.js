60909af6048f2b104eab47defba40f218cf3bd45
githinjisamson1 <githinjisamson148@gmail.com>
2024-01-20 18:06:20 +0300

    Wrap entire app in PizzasProvider

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 import { BrowserRouter } from "react-router-dom";
+import { PizzasProvider } from "./context/pizzasContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <PizzasProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PizzasProvider>
   </>
 );
