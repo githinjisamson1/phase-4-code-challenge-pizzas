7b45a0050fa43f14d9d96a2bc9741158d2aac8f2
githinjisamson1 <githinjisamson148@gmail.com>
2024-01-20 17:42:20 +0300

    Add SearchForm in Home

---
+++ client/src/components/searchForm/SearchForm.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./searchForm.css"
+
+const SearchForm = () => {
+  return (
+    <div>SearchForm</div>
+  )
+}
+
+export default SearchForm
--- client/src/pages/homePage/Home.jsx
+++ client/src/pages/homePage/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import "./home.css"
+import SearchForm from '../../components/searchForm/SearchForm'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <SearchForm/>
+    </div>
   )
 }
 
