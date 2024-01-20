044d9221284ef0abcd5d3b530fa11abdd1ec7d20
githinjisamson1 <githinjisamson148@gmail.com>
2024-01-20 18:07:06 +0300

    Complete SearchForm

--- client/src/components/searchForm/SearchForm.jsx
+++ client/src/components/searchForm/SearchForm.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import "./searchForm.css"
+import "./searchForm.css";
+import { useGlobalPizzasContext } from "../../context/pizzasContext";
 
 const SearchForm = () => {
-  return (
-    <div>SearchForm</div>
-  )
-}
+  const { searchTerm, setSearchTerm } = useGlobalPizzasContext();
 
-export default SearchForm
+  const handleSearchTermChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="search-form">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="search-term"
+          id="search-term"
+          value={searchTerm}
+          onChange={(e) => {
+            handleSearchTermChange(e);
+          }}
+          placeholder="Search your favorite pizza"
+        />
+      </form>
+    </div>
+  );
+};
+
+export default SearchForm;
