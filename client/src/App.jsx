4133d7b86cd6bb1229681c2027e70f25338ff7b8
githinjisamson1 <githinjisamson148@gmail.com>
2024-01-20 21:58:01 +0300

    Configure /pizzas/:id route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/homePage/Home";
 import About from "./pages/aboutPage/About";
 import Error from "./pages/errorPage/Error";
 import Navbar from "./components/navbar/Navbar";
+import SinglePizza from "./pages/singlePizzaPage/SinglePizza";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/about" element={<About />} />
+        <Route exact path="/pizzas/:id" element={<SinglePizza />} />
         <Route exact path="*" element={<Error />} />
       </Routes>
     </div>
