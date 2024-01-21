18723be2aff947c656fecfbed220f01c104f8b33
githinjisamson1 <githinjisamson148@gmail.com>
2024-01-21 13:01:49 +0300

    Complete sample about

--- client/src/pages/aboutPage/About.jsx
+++ client/src/pages/aboutPage/About.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import "./about.css"
+import React from "react";
+import "./about.css";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="about">
+      <p>
+        Indulge in a symphony of flavors at The Pizza Inn, where passion meets
+        perfection in every bite. Our handcrafted pizzas boast a tantalizing
+        blend of premium ingredients, crispy crusts, and savory sauces. Immerse
+        yourself in a culinary journey that transcends ordinary, making every
+        moment at Slice Haven a slice of heaven!
+      </p>
+    </div>
+  );
+};
 
-export default About
+export default About;
