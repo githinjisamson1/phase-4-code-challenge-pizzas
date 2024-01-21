1e0bac4b4ca070f11f011191aebfd6cba4d9306e
githinjisamson1 <githinjisamson148@gmail.com>
2024-01-21 12:45:04 +0300

    Complete Error component

--- client/src/pages/errorPage/Error.jsx
+++ client/src/pages/errorPage/Error.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import "./error.css"
+import React from "react";
+import "./error.css";
+import { useNavigate } from "react-router-dom";
 
 const Error = () => {
-  return (
-    <div>Error</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default Error
+  return (
+    <div className="error">
+      <h1 className="error-msg">Oopsie! Could not find resource!</h1>
+      <button
+        className="error-btn"
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        Back Home
+      </button>
+    </div>
+  );
+};
+
+export default Error;
